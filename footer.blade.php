9323e27bb2d3099ece6d51e4eaf3c4630017c92c
tiromo <85551013+tiromo@users.noreply.github.com>
2025-04-01 00:08:35 +0900

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDoList</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="absolute inset-x-0 h-16 mb-8 bg-slate-800 text-3xl border-t">
+        <div>    
+            ToDoList
+    </div>
+</body>
---
+++ header.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDoList</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="absolute inset-x-0 top-0 h-16 bg-slate-800 text-3xl flex justify-between items-center">
+        <div class="text-white">    
+            ToDoList
+        </div>
+        <div class="flex space-x-5">
+            <div class="text-white">
+                <a href="toppage.html" class="toppage">トップページ</a>
+            </div>
+            <div class="text-white">
+                <a href="mypage.html" class="mylist">マイリスト</a>
+            </div>
+            <div class="text-white">
+                <a href="toppage.html" class="logout">ログアウト</a>
+        
+            </div>    
+    
+        </div>
+    </div>
+</body>
